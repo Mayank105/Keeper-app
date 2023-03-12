5910d54299e00f6a2115db29a4894beb0f21a9ec
Mayank105 <91548436+Mayank105@users.noreply.github.com>
2023-03-12 15:56:27 +0530

    Updates

---
+++ precache-manifest.1aefa0803706afd86d511aec7963478c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc901c7227ee69b8d9a",
+    "url": "/react-website/static/js/main.5fc901c7.chunk.js"
+  },
+  {
+    "revision": "153511d61e2add995e57",
+    "url": "/react-website/static/js/1.153511d6.chunk.js"
+  },
+  {
+    "revision": "4bd2eb48bc79ce71ea7a",
+    "url": "/react-website/static/js/runtime~main.4bd2eb48.js"
+  },
+  {
+    "revision": "19e786591cb6ddef403ba88ac10cc571",
+    "url": "/react-website/index.html"
+  }
+];
